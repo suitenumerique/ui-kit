c479554a6998596541d45f6f72754ad0b857a290
Nathan Vasse <nathan.vasse@gmail.com>
2025-02-25 16:22:33 +0100

    ✨(i18n) allow custom locales
    We faced an issue: it is not possible to import cunningham locales
    directly because of the lack of an export. Knowing we want to add
    our custom translations to it, I added this approach to import the
    local to the src folder and then a custom CunninghamProvider than
    just inject the custom translation into the real CunninghamProvider.
    The custom translations of this package should be put inside
    src/locales/en-US.json or fr.json.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import "./library.scss";
 
 export * from "./components/button/Button";
+export * from "./components/Provider/Provider";
+export * from "./locales/Locale";
 
 export { default as cunninghamConfig } from "../cunningham";
---
+++ src/utils/objects.ts
@@ -0,0 +1,21 @@
+function isObject(item: unknown) {
+  return item && typeof item === "object" && !Array.isArray(item);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function deepMerge(target: any, ...sources: any[]): any {
+  if (!sources.length) return target;
+  const source = sources.shift();
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) Object.assign(target, { [key]: {} });
+        deepMerge(target[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
+  return deepMerge(target, ...sources);
+}
