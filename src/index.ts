7fa18a52d8760c8f6e5e82da7a3979a6f44a04ae
Nathan Panchout <nathanpanchout@hotmail.com>
2025-02-28 17:52:52 +0100

    ✨(components) add DropdownMenu component
    - Created DropdownMenu component with flexible option configuration
    - Added stories to showcase various dropdown menu scenarios
    - Implemented useDropdownMenu hook for managing dropdown state
    - Added SCSS styling for dropdown menu and menu items
    - Exported new component and related types

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import "./library.scss";
 
 export * from "./components/Provider/Provider";
 export * from "./locales/Locale";
+export * from "./components/button";
+export * from "./components/dropdown-menu";
 
 export { default as cunninghamConfig } from "../cunningham";
