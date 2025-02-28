9d4015a769e3f250812d9ff477bc695445d98eb0
Nathan Vasse <nathan.vasse@gmail.com>
2025-02-28 16:36:56 +0100

    ✨(component) add DataGrid
    This is a very very first customized version of the DataGrid. We'll
    need to iterate over soon.

---
+++ src/components/datagrid/datagrid.stories.tsx
@@ -0,0 +1,89 @@
+import type { Meta } from "@storybook/react";
+import databaseCars from "./resources/databaseCars.json";
+
+import { Button, DataGrid, SimpleDataGrid } from "@openfun/cunningham-react";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Components/Datagrid",
+  component: DataGrid,
+  tags: ["autodocs"],
+
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+} satisfies Meta<typeof DataGrid>;
+
+export default meta;
+
+export const Empty = () => {
+  return (
+    <DataGrid
+      columns={[
+        {
+          field: "firstName",
+          headerName: "First name",
+          highlight: true,
+        },
+      ]}
+      rows={[]}
+    />
+  );
+};
+
+export const EmptyCustomWithButton = () => {
+  return (
+    <DataGrid
+      columns={[
+        {
+          field: "firstName",
+          headerName: "First name",
+          highlight: true,
+        },
+      ]}
+      rows={[]}
+      emptyPlaceholderLabel="This table is empty, create the first object"
+      emptyCta={
+        <Button
+          color="primary"
+          icon={<span className="material-icons">add</span>}
+        >
+          Create object
+        </Button>
+      }
+    />
+  );
+};
+
+export const ClientSideWithPagination = () => {
+  return (
+    <>
+      <SimpleDataGrid
+        columns={[
+          {
+            field: "carName",
+            headerName: "Nom",
+            enableSorting: false,
+          },
+          {
+            field: "year",
+            headerName: "Année",
+          },
+          {
+            field: "price",
+            headerName: "Prix",
+            highlight: true,
+          },
+        ]}
+        rows={databaseCars}
+        defaultPaginationParams={{
+          pageSize: 5,
+        }}
+        defaultSortModel={[
+          {
+            field: "price",
+            sort: "desc",
+          },
+        ]}
+      />
+    </>
+  );
+};
