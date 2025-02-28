cdd3794f0a0a31c25090d19cfb027512419cff56
Nathan Vasse <nathan.vasse@gmail.com>
2025-02-28 16:39:09 +0100

    📦️(various) update import and build tokens
    Title says it all here.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "./index.scss";
 import "./library.scss";
 
-
 export * from "./components/button/Button";
+export * from "./components/form/label/label";
 
 export { default as cunninghamConfig } from "../cunningham";
