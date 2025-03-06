41e2da0ffb9bd04d06b1036b117cadee6cd7f7d6
Nathan Panchout <nathanpanchout@hotmail.com>
2025-03-06 14:47:28 +0100

    ✨(components) add Draggable and Droppable components
    - Created a generic Draggable component with opacity management
    while dragging
    - Added a Droppable component with hover event support
    - Updated the main layout to use flexbox

---
+++ src/components/dnd/Droppable.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Data, useDroppable } from "@dnd-kit/core";
+import { useEffect } from "react";
+
+type DroppableProps<T> = {
+  id: string;
+  onOver?: (isOver: boolean, data?: Data<T>) => void;
+  data?: Data<T>;
+  children: React.ReactNode;
+};
+
+export const Droppable = <T,>(props: DroppableProps<T>) => {
+  const { isOver, setNodeRef, active } = useDroppable({
+    id: props.id,
+    data: props.data,
+  });
+
+  useEffect(() => {
+    props.onOver?.(isOver, props.data);
+  }, [isOver, props.data, props.onOver]);
+
+  return <div ref={setNodeRef}>{props.children}</div>;
+};
