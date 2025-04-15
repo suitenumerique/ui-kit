3113af523591abfaf9dd9d38042de301570587c9
Nathan Panchout <nathanpanchout@hotmail.com>
2025-04-15 09:40:44 +0200

    ✨(components) add topMessage prop to DropdownMenu
    Added a new optional prop `topMessage` to the DropdownMenu component.
    This allows for displaying a message at the top of the dropdown menu.
    Additionally, updated styles to accommodate the new message display.

--- src/components/dropdown-menu/dropdown.stories.tsx
+++ src/components/dropdown-menu/dropdown.stories.tsx
@@ -17,58 +17,58 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+const options = [
+  {
+    icon: <span className="material-icons">info</span>,
+    label: "Informations",
+    value: "info",
+  },
+  {
+    icon: <span className="material-icons">group</span>,
+    label: "Partager",
+    callback: () => alert("Partager"),
+  },
+  {
+    icon: <span className="material-icons">download</span>,
+    label: "Télécharger",
+    value: "download",
+    showSeparator: true,
+  },
+  {
+    icon: <span className="material-icons">edit</span>,
+    label: "Renommer",
+    value: "rename",
+    showSeparator: true,
+  },
+  {
+    icon: <span className="material-icons">arrow_forward</span>,
+    label: "Déplacer",
+    value: "move",
+  },
+  {
+    icon: <span className="material-icons">arrow_back</span>,
+    label: "Dupliquer",
+    value: "duplicate",
+  },
+  {
+    icon: <span className="material-icons">add</span>,
+    isDisabled: true,
+    label: "Crééer un raccourci",
+    value: "create-shortcut",
+    showSeparator: true,
+  },
+  {
+    icon: <span className="material-icons">delete</span>,
+    label: "Supprimer",
+    value: "delete",
+    showSeparator: true,
+  },
+];
+
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default: Story = {
   args: {
-    options: [
-      {
-        icon: <span className="material-icons">info</span>,
-        label: "Informations",
-        value: "info",
-      },
-      {
-        icon: <span className="material-icons">group</span>,
-        label: "Partager",
-        callback: () => alert("Partager"),
-      },
-      {
-        icon: <span className="material-icons">download</span>,
-        label: "Télécharger",
-        value: "download",
-
-        showSeparator: true,
-      },
-      {
-        icon: <span className="material-icons">edit</span>,
-        label: "Renommer",
-        value: "rename",
-
-        showSeparator: true,
-      },
-      {
-        icon: <span className="material-icons">arrow_forward</span>,
-        label: "Déplacer",
-        value: "move",
-      },
-      {
-        icon: <span className="material-icons">arrow_back</span>,
-        label: "Dupliquer",
-        value: "duplicate",
-      },
-      {
-        icon: <span className="material-icons">add</span>,
-        isDisabled: true,
-        label: "Crééer un raccourci",
-        value: "create-shortcut",
-        showSeparator: true,
-      },
-      {
-        icon: <span className="material-icons">delete</span>,
-        label: "Supprimer",
-        value: "delete",
-        showSeparator: true,
-      },
-    ],
+    options: options,
   },
   render: (args) => {
     const { isOpen, setIsOpen } = useDropdownMenu();
@@ -79,6 +79,48 @@ export const Default: Story = {
           setSelectedValues([value]);
         }}
         options={args.options}
+        isOpen={isOpen}
+        onOpenChange={setIsOpen}
+        selectedValues={selectedValues}
+      >
+        <div style={{ display: "flex", gap: "10px" }}>
+          <Button onClick={() => setIsOpen(!isOpen)}>Open</Button>
+        </div>
+      </DropdownMenu>
+    );
+  },
+};
+
+export const WithTopMessage: Story = {
+  args: {
+    options: [
+      {
+        label: "Administrator",
+        value: "admin",
+        isDisabled: true,
+      },
+      {
+        label: "Editor",
+        value: "editor",
+        isDisabled: true,
+      },
+      {
+        label: "Viewer",
+        value: "viewer",
+        isDisabled: true,
+      },
+    ],
+    topMessage: "You don't have permission to modify these roles",
+  },
+  render: (args) => {
+    const { isOpen, setIsOpen } = useDropdownMenu();
+    const [selectedValues, setSelectedValues] = useState<string[]>(["info"]);
+    return (
+      <DropdownMenu
+        {...args}
+        onSelectValue={(value) => {
+          setSelectedValues([value]);
+        }}
         isOpen={isOpen}
         onOpenChange={setIsOpen}
         selectedValues={selectedValues}
