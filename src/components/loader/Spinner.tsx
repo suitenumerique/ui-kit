366478c518d0d4ac718d7c24fb87b8043bbcf747
Nathan Panchout <nathanpanchout@hotmail.com>
2025-04-15 09:41:20 +0200

    ✨(components) change SpinnerProps interface to type
    Updated the SpinnerProps from an interface to a type definition for
    better consistency and clarity in the codebase and delete the warning
    from storybook

--- src/components/loader/Spinner.tsx
+++ src/components/loader/Spinner.tsx
@@ -1,6 +1,6 @@
-interface SpinnerProps {
+type SpinnerProps = {
   size?: "sm" | "md" | "lg" | "xl";
-}
+};
 
 export const Spinner = ({ size = "sm" }: SpinnerProps) => {
   return <div className={["c__spinner", size].join(" ")} />;
