76a220997e3808043a3f0be8da97f318a0661678
Manuel Raynaud <manu@raynaud.io>
2025-07-31 15:01:24 +0200

    ✨(components) make Footer configurable
    We want to be able to entirely configure the Footer component. Props are
    now defined for the ExternalLInks, LegalLinks, License and the logo.

--- src/components/footer/Footer.stories.tsx
+++ src/components/footer/Footer.stories.tsx
@@ -14,4 +14,48 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const WithoutProps: Story = {};
+
+export const WithProps: Story = {
+  args: {
+    externalLinks: [
+      {
+        label: "legifrance.gouv.fr",
+        href: "https://legifrance.gouv.fr/",
+      },
+      {
+        label: "info.gouv.fr",
+        href: "https://info.gouv.fr/",
+      },
+      {
+        label: "service-public.fr",
+        href: "https://service-public.fr/",
+      },
+      {
+        label: "data.gouv.fr",
+        href: "https://data.gouv.fr/",
+      },
+    ],
+    legalLinks: [
+      {
+        label: "Legal Mentions",
+        href: "/legal-notice",
+      },
+      {
+        label: "Personal Data and cookies",
+        href: "/personal-data-cookies",
+      },
+      {
+        label: "Accessibility: non-compliant",
+        href: "/accessibility",
+      },
+    ],
+    license: {
+      label: "Unless otherwise stated, all content on this site is under",
+      link: {
+        label: "licence etalab-2.0",
+        href: "https://github.com/etalab/licence-ouverte/blob/master/LO.md",
+      },
+    },
+  },
+};
