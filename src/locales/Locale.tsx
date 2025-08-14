422166b00e5ef7bca096e20c2527cd8c63515e95
jbpenrath <jb.penrath@gmail.com>
2025-08-14 18:14:12 +0200

    🏷️(type) add translation key auto completion
    Create type for translation in order to improve dx when user wants to override
    some translations

--- src/locales/Locale.tsx
+++ src/locales/Locale.tsx
@@ -3,9 +3,7 @@ import { default as enUS } from "./en-US.json";
 import { default as frFR } from "./fr-FR.json";
 import { deepMerge } from "../utils/objects";
 
-export function getLocales() {
-  return {
+export const locales = {
     "en-US": deepMerge(originalEnUS, enUS),
     "fr-FR": deepMerge(originalFrFR, frFR),
-  };
 }
