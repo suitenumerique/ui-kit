ed9d504cef49ad500c0a1eda0ece7cdf7845c10f
Nathan Vasse <nathan.vasse@gmail.com>
2025-10-01 16:51:36 +0200

    🐛(badge) fix the export
    The component was not importable from consumer apps due to the
    way the export was declared.

--- src/components/badge/badge.stories.tsx
+++ src/components/badge/badge.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import Badge from './badge';
+import { Badge } from './badge';
 
 const meta: Meta<typeof Badge> = {
   title: 'Components/Badge',
