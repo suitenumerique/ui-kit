afdf5e80c9467f74e2f7c4f3bc2650bcfe8f1a65
jbpenrath <jb.penrath@gmail.com>
2025-12-15 10:38:20 +0100

    🧑‍💻(storybook) allow to switch between existing themes
    Tweak storybook to add a background selector in charge to switch of themes.

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,12 +3,23 @@ import "./../src/index.scss";
 import "./../src/styles/fonts.scss";
 import "./../src/style-stories.scss";
 import type { Preview } from "@storybook/react";
+import { DocsContainer } from "@storybook/blocks";
 import React from "react";
+import { BACKGROUND_COLOR_TO_THEME, getThemeFromGlobals, Themes, themes } from "./theme";
+
+const DocsWithTheme = (props) => {
+  const theme = getThemeFromGlobals(props.context.store.userGlobals.globals);
+  return (
+    <CunninghamProvider theme={theme}>
+      <DocsContainer {...props} theme={themes[theme]} />
+    </CunninghamProvider>
+  );
+};
 
 const preview: Preview = {
   decorators: [
-    (Story) => (
-      <CunninghamProvider currentLocale="en-US">
+    (Story, context) => (
+      <CunninghamProvider currentLocale="en-US" theme={getThemeFromGlobals(context.globals)}>
         <div>
           <Story />
         </div>
@@ -17,18 +28,20 @@ const preview: Preview = {
   ],
   parameters: {
     backgrounds: {
-      values: [
-        // 👇 Add your own
-        { name: "Gray", value: "#F7F9F2" },
-      ],
-      // 👇 Specify which background is shown by default
-      default: "light",
+      default: null,
+      values: Object.entries(BACKGROUND_COLOR_TO_THEME).map(([key, value]) => ({
+        name: Themes[value][1],
+        value: key,
+      })),
     },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    docs: {
+      container: DocsWithTheme,
     },
   },
 };
